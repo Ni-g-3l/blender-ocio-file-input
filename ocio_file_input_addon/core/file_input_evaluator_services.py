04b70003253d5da665863fe8afc0cfece28bb0bc
Nig3l <nig3lpro@gmail.com>
2024-03-05 20:07:35 +0100

    feat: Create file evaluator service

---
+++ ocio_file_input_addon/core/file_input_evaluator_services.py
@@ -0,0 +1,30 @@
+import collections
+import fnmatch
+import os
+
+
+FileInputRule = collections.namedtuple(
+    "FileInputRule", "name extension pattern colorspace"
+)
+
+class FileInputEvaluatorService:
+
+    DEFAULT_RULE = FileInputRule("Default", "*", "*", "raw")
+
+    def __init__(self, rules:list = None) -> None:
+        if not rules:
+            rules = []
+        self._rules = rules
+
+    def eval(self, filepath) -> str:
+        for rule in reversed(self._rules):
+            if self._file_is_matching_rule(rule, filepath):
+                    return rule.colorspace
+        return self.DEFAULT_RULE.colorspace
+    
+    def _file_is_matching_rule(self, rule, filepath):
+        file_ext = os.path.splitext(filepath)[-1]
+        file_name = os.path.splitext(os.path.basename(filepath))[0]
+        ext_match = rule.extension == self.DEFAULT_RULE.extension or rule.extension == file_ext
+        name_match = rule.pattern == self.DEFAULT_RULE.pattern or fnmatch.fnmatch(file_name, rule.pattern)
+        return ext_match and name_match
--- tests/test_ocio_file_input_addon.py
+++ tests/test_ocio_file_input_addon.py
@@ -1,9 +1,45 @@
 import unittest
 
+from ocio_file_input_addon.core.file_input_evaluator_services import FileInputEvaluatorService, FileInputRule
+
 class Test_ocio_file_input_addon(unittest.TestCase):
 
-    def test_dummy(self):
-        self.assertTrue(False)
+    def test_return_default_rule_on_empty_rule_list(self):
+        evaluator = FileInputEvaluatorService()
+        self.assertEqual("raw", evaluator.eval("/tmp/salut.exr"))
+        evaluator = FileInputEvaluatorService([])
+        self.assertEqual("raw", evaluator.eval("/tmp/salut.exr"))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_return_colorspace_based_on_extension(self):
+        ext_rule = FileInputRule("EXR Rule", ".exr", "*", "srgb")
+        evaluator = FileInputEvaluatorService([ext_rule])
+        self.assertEqual("srgb", evaluator.eval("/tmp/salut.exr"))
+        ext_rule = FileInputRule("PNG Rule", ".png", "*", "srgb")
+        evaluator = FileInputEvaluatorService([ext_rule])
+        self.assertEqual("raw", evaluator.eval("/tmp/salut.exr"))
+
+    def test_return_colorspace_based_on_name(self):
+        name_rule = FileInputRule("SRGB File name Rule", "*", "*-srgb", "srgb")
+        evaluator = FileInputEvaluatorService([name_rule])
+        self.assertEqual("srgb", evaluator.eval("/tmp/salut-srgb.exr"))
+        evaluator = FileInputEvaluatorService([name_rule])
+        self.assertEqual("raw", evaluator.eval("/tmp/salut-linear.exr"))
+
+    def test_return_colorspace_with_multiple_rules_based_on_order(self):
+        evaluator = FileInputEvaluatorService([
+            FileInputRule("SRGB File name Rule", "*", "*-srgb", "srgb"),
+            FileInputRule("EXR Rule", ".exr", "*", "acescg")
+        ])
+        self.assertEqual("acescg", evaluator.eval("/tmp/salut-srgb.exr"))
+
+        evaluator = FileInputEvaluatorService([
+            FileInputRule("SRGB File name Rule", "*", "*-srgb", "srgb"),
+            FileInputRule("EXR Rule", ".exr", "*", "acescg")
+        ])
+        self.assertEqual("srgb", evaluator.eval("/tmp/salut-srgb.tiff"))
+        
+        evaluator = FileInputEvaluatorService([
+            FileInputRule("SRGB File name Rule", "*", "*-srgb", "srgb"),
+            FileInputRule("EXR Rule", ".exr", "*", "acescg")
+        ])
+        self.assertEqual("raw", evaluator.eval("/tmp/salut-linear.jpeg"))
