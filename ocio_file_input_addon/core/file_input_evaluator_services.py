5eaadf2d77ae8960749950e9caad35846a0d2213
Nig3l <nig3lpro@gmail.com>
2024-04-15 18:44:09 +0200

    refactor(service): Store basename in var

--- ocio_file_input_addon/core/file_input_evaluator_services.py
+++ ocio_file_input_addon/core/file_input_evaluator_services.py
@@ -17,14 +17,15 @@ class FileInputEvaluatorService:
         self._rules = rules
 
     def eval(self, filepath) -> str:
-        for rule in reversed(self._rules):
+        for rule in self._rules:
             if self._file_is_matching_rule(rule, filepath):
-                    return rule.colorspace
+                return rule.colorspace
         return self.DEFAULT_RULE.colorspace
     
     def _file_is_matching_rule(self, rule, filepath):
-        file_ext = os.path.splitext(filepath)[-1]
-        file_name = os.path.splitext(os.path.basename(filepath))[0]
+        file_basename = os.path.basename(filepath)
+        file_ext = os.path.splitext(file_basename)[-1]
+        file_name = os.path.splitext(file_basename)[0]
         ext_match = rule.extension == self.DEFAULT_RULE.extension or rule.extension == file_ext
         name_match = rule.pattern == self.DEFAULT_RULE.pattern or fnmatch.fnmatch(file_name, rule.pattern)
         return ext_match and name_match
